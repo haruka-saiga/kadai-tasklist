2fad0be45c7ed257d727f1f4687a0706718e40ef
haruka.saiga <ec2-user@ip-172-31-26-50.us-east-2.compute.internal>
2018-05-30 06:47:30 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,11 +3,15 @@
 @section('content')
 
     <h1>タスク新規作成ページ</h1>
+    
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
+
+{!! Form::label('title', '状況:') !!}
+        {!! Form::text('status') !!}
 
         {!! Form::submit('投稿') !!}
 
